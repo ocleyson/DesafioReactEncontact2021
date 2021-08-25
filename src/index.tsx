1c423a2075e13a4e08b64bfa4f019182518b7cc0
Cleyson L <clbbdev@gmail.com>
2021-08-24 23:01:48 -0300

    add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import reportWebVitals from "./reportWebVitals";
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App>
+        <Switch>
+          <Route exact path="/">
+            <h1>home</h1>
+          </Route>
+        </Switch>
+      </App>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
